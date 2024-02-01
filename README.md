276a81fcf9e6895d0a6a451f6777094b900bc7fc
Darren Pavel <darren.pavel@gmail.com>
2024-01-31 23:30:54 -0800

    ✨ feat(Added loading on popup load. Added saving a link to the Google Sheet URL.):
    Refactored some code as well as added the above features.
    
    This still requires creating a Google App script and changing the code to make it work.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Job-Application-Tracking-
 Scrape your Job Posting Data into a Google Sheet
+
+## Linking to a Google Sheet
+Within Google Sheets, you will need to:
+1. click Extensions
+2. click App Scripts
+3. Do some voodoo I haven't looked at yet
+4. change the links in the top of popup.js to match the links to your sheets
+   
+### Future QOL changes
+This can be done by importing the Google API's:
+
+```<script src="https://apis.google.com/js/api.js"></script>```
+
+And then using implicit authentication to verify the user and create the app script.
+
+However, this also requires creating and saving an API key to access the API.
--- popup.js
+++ popup.js
@@ -1,147 +1,201 @@
-var postURL = 'https://script.google.com/macros/s/AKfycbz9vrxvqiHk9gFLxFU4FTsM2tHTo1leh1CYxVO3foM0BJMN7Srr1ohSAif1ftBM0PI/exec';
-var getURL = 'https://script.google.com/macros/s/AKfycbwmeb4AKGSoPc7-7URWWil_BpDWXueMiMmAcE6RnQWsKiabSB-1VeQhhucVLyyvWpXW/exec';
-var getdailyURL = 'https://script.google.com/macros/s/AKfycbwZSXOZTzz--npozzkW2FOiXBzd2HMV4hPh8Xy3w4u4tH3F-tACDydqTylDfWi6yS8h/exec'
+var postURL =
+  'https://script.google.com/macros/s/AKfycbz9vrxvqiHk9gFLxFU4FTsM2tHTo1leh1CYxVO3foM0BJMN7Srr1ohSAif1ftBM0PI/exec';
+var getURL =
+  'https://script.google.com/macros/s/AKfycbwmeb4AKGSoPc7-7URWWil_BpDWXueMiMmAcE6RnQWsKiabSB-1VeQhhucVLyyvWpXW/exec';
+var getdailyURL =
+  'https://script.google.com/macros/s/AKfycbwZSXOZTzz--npozzkW2FOiXBzd2HMV4hPh8Xy3w4u4tH3F-tACDydqTylDfWi6yS8h/exec';
 
-document.addEventListener('DOMContentLoaded', function() {
-    var loadDataButton = document.getElementById('loadDataButton');
-    loadDataButton.addEventListener('click', function() {
-        loadDataButton.style.display = 'none';
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: 'loadData' },
+      function (response) {
+        if (response) {
+          updateForm(response);
+        }
+      }
+    );
+  });
 
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "loadData"}, function(response) {
-                if (response) {
-                    createForm(response);
-                }
-            });
-        });
-    });
+  chrome.storage.local.get(['sheetURL'], function (result) {
+    if (result.sheetURL) {
+      createSheetLink(result.sheetURL);
+    }
+  });
+
+  toggleCogFunction();
 });
 
-function createForm(response) {
-    var resultDiv = document.getElementById('result');
-    var form = document.createElement('form');
-    form.id = 'dataForm';
-    form.innerHTML = `
-        <label for="jobTitle">Job Title:</label>
-        <input type="text" name="jobTitle" value="${response.jobTitle}"><br>
-        <label for="company">Company:</label>
-        <input type="text" name="company" value="${response.company}"><br>
-        <label for="unknownInput">Unknown Input:</label>
-        <input type="text" name="unknownInput" value="${response.unknownInput}"><br>
-        <label for="applicationDateTime">Application Date and Time:</label>
-        <input type="text" name="applicationDateTime" value="${response.applicationDateTime}"><br>
-        <label for="url">URL:</label>
-        <input type="text" name="url" value="${response.url}" readonly><br>
-    `;
+function updateSheet() {
+  const sheetElement = document.querySelector('#sheet');
+  if (sheetElement.firstChild) {
+    sheetElement.removeChild(sheetElement.firstChild);
+  }
 
-    var submitButton = document.createElement('input');
-    submitButton.type = 'submit';
-    submitButton.value = 'Submit';
-    submitButton.id = 'submitButton';
-    form.appendChild(submitButton);
+  const sheetInput = document.createElement('input');
+  sheetInput.type = 'text';
+  sheetInput.id = 'sheetUrl';
+  sheetInput.onkeydown = function (event) {
+    if (event.key === 'Enter') {
+      sheetURL = sheetInput.value;
+      saveSheet();
+    }
+  };
+  sheetElement.appendChild(sheetInput);
 
-    resultDiv.appendChild(form);
+  toggleCogFunction(true);
+}
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        submitFormData();
-        appendSubmissionMessage();
-        removeSubmitButton();
-        fetchTotalJobsApplied(); 
-        fetchTotalJobsAppliedToday();
-    });
+function saveSheet() {
+  const sheetElement = document.querySelector('#sheet');
+  const sheetURL = sheetElement.querySelector('input').value;
+  console.log(sheetURL);
+
+  if (sheetElement.firstChild) {
+    sheetElement.removeChild(sheetElement.firstChild);
+  }
+  if (!sheetURL) {
+    const textElement = document.createElement('span');
+    textElement.textContent = 'No Google Sheet URL provided';
+    sheetElement.appendChild(textElement);
+  } else {
+    createSheetLink(sheetURL);
+
+    chrome.runtime.sendMessage({ action: 'loadSheet', sheetURL: sheetURL });
+  }
+
+  toggleCogFunction(false);
+}
+
+function createSheetLink(sheetURL) {
+  const sheetElement = document.querySelector('#sheet');
+
+  const link = document.createElement('a');
+  link.text = 'Open Google Sheet';
+  link.href = sheetURL;
+  link.target = '_blank';
+  sheetElement.appendChild(link);
+}
+
+function toggleCogFunction(save = false) {
+  const clickFunction = save ? saveSheet : updateSheet;
+  const oldFunction = save ? updateSheet : saveSheet;
+
+  const cog = document.querySelector('#linkSheet');
+  cog.removeEventListener('click', oldFunction);
+  cog.addEventListener('click', clickFunction);
+}
+
+function updateForm(response) {
+  const form = document.querySelector('#jobForm');
+  for (const id in response) {
+    document.querySelector(`input[name="${id}"]`).value = response[id];
+  }
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    submitFormData();
+    appendSubmissionMessage();
+    removeSubmitButton();
+    fetchTotalJobsApplied();
+    fetchTotalJobsAppliedToday();
+  });
 }
 
 function submitFormData() {
-    var form = document.getElementById('dataForm');
-    var formData = new FormData(form);
-    var object = {};
-    formData.forEach((value, key) => { object[key] = value; });
+  var form = document.getElementById('jobForm');
+  var formData = new FormData(form);
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
 
-    // Fetch request to send the JSON data using baseURL with action parameter
-    fetch(postURL + '?action=addUser', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(object)
+  // Fetch request to send the JSON data using baseURL with action parameter
+  fetch(postURL + '?action=addUser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(object),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Response:', data);
+      fetchTotalJobsApplied(); // Fetch total jobs applied after successful submission
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Response:', data);
-        fetchTotalJobsApplied(); // Fetch total jobs applied after successful submission
-    })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error('Error:', error));
 }
 
 function appendSubmissionMessage() {
-    var resultDiv = document.getElementById('result');
-    var successMessage = document.createElement('p');
-    successMessage.textContent = 'Data Submitted';
-    resultDiv.appendChild(successMessage);
+  updateResult('Data Submitted');
 }
 
 function removeSubmitButton() {
-    var submitButton = document.getElementById('submitButton');
-    if (submitButton) {
-        submitButton.remove();
-    }
+  var submitButton = document.getElementById('submitButton');
+  if (submitButton) {
+    submitButton.remove();
+  }
 }
 
 function fetchTotalJobsApplied() {
-    // Fetch request using baseURL with a different action parameter
-    fetch(getURL + '?action=getUsers', {
-        method: 'GET'
+  // Fetch request using baseURL with a different action parameter
+  fetch(getURL + '?action=getUsers', {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      logTotalJobs(data);
+      console.log(data);
+      // console.alert(data) // Correct this to console.log or alert
     })
-    .then(response => response.json())
-    .then(data => {
-        logTotalJobs(data);
-        console.log(data); 
-        // console.alert(data) // Correct this to console.log or alert
-    })
-    .catch(error => console.error('Error fetching total jobs:', error));
+    .catch((error) => console.error('Error fetching total jobs:', error));
 }
 
 function logTotalJobs(data) {
-    var resultDiv = document.getElementById('result');
-    var jobsMessage = document.createElement('p');
+  let jobsMessage = '';
 
-    // Check if the data is in the expected nested array format
-    if (data && Array.isArray(data) && data[0] && Array.isArray(data[0])) {
-        let totalJobs = data[0][0];
-        jobsMessage.textContent = `${totalJobs} jobs applied to in total`;
-    } else {
-        jobsMessage.textContent = "Unable to find total jobs data";
-    }
+  // Check if the data is in the expected nested array format
+  if (data && Array.isArray(data) && data[0] && Array.isArray(data[0])) {
+    const totalJobs = data[0][0];
+    jobsMessage = `${totalJobs} jobs applied to in total`;
+  } else {
+    jobsMessage = 'Unable to find total jobs data';
+  }
 
-    resultDiv.appendChild(jobsMessage);
+  updateResult(jobsMessage);
 }
 
 function fetchTotalJobsAppliedToday() {
-    fetch(getdailyURL+'?action=getDailyTotal', {
-        method: 'GET'
+  fetch(getdailyURL + '?action=getDailyTotal', {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Daily Total Response:', data); // Log the response
+      logTotalJobsToday(data);
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Daily Total Response:', data); // Log the response
-        logTotalJobsToday(data);
-    })
-    .catch(error => {
-        console.error('Error fetching total jobs today:', error); // Log any errors
+    .catch((error) => {
+      console.error('Error fetching total jobs today:', error); // Log any errors
     });
 }
 
 function logTotalJobsToday(data) {
-    var resultDiv = document.getElementById('result');
-    var jobsMessage = document.createElement('p');
+  let jobsMessage = '';
 
-    if (data && Array.isArray(data) && data[0] && Array.isArray(data[0])) {
-        let totalJobsToday = data[0][0];
-        jobsMessage.textContent = `${totalJobsToday} jobs applied to in total today`;
-    } else {
-        console.error("Data format is not as expected:", data); // Log unexpected data format
-        jobsMessage.textContent = "Unable to find total jobs data for today";
-    }
+  if (data && Array.isArray(data) && data[0] && Array.isArray(data[0])) {
+    const totalJobsToday = data[0][0];
+    jobsMessage = `${totalJobsToday} jobs applied to in total today`;
+  } else {
+    console.error('Data format is not as expected:', data); // Log unexpected data format
+    jobsMessage = 'Unable to find total jobs data for today';
+  }
 
-    resultDiv.appendChild(jobsMessage);
+  updateResult(jobsMessage);
+}
+
+function updateResult(message) {
+  const resultDiv = document.getElementById('result');
+  const messageDiv = document.createElement('p');
+  resultDiv.appendChild(messageDiv);
 }
