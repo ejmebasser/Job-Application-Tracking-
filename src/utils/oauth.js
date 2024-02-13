cf5325b1ac276a479da71344815e11e892406fa6
Darren Pavel <darren.pavel@gmail.com>
2024-02-12 17:09:18 -0800

    ✨ feat(OAuth): Add setup for OAuth
    Inclued changes to package.json and manifest to handle transition to OAuth. Also add oauth.js file to begin setting up oauth.

--- src/utils/oauth.js
+++ src/utils/oauth.js
@@ -1,7 +1,15 @@
 window.onload = function () {
   document.querySelector('button').addEventListener('click', function () {
     chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      console.log(token);
+      let init = {
+        method: 'GET',
+        async: true,
+        Headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json',
+        },
+        contentType: 'json',
+      };
     });
   });
 };
