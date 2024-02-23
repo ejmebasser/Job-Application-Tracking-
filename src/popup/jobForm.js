7e119d38b447ee4faf9b9b7ab10a188cc42a932e
Emil Mebasser <ejmebasser@gmail.com>
2024-02-23 13:52:32 -0800

    now hides job when saved from popup

--- src/popup/jobForm.js
+++ src/popup/jobForm.js
@@ -53,6 +53,7 @@ export default class JobForm {
    * Handles what happens when we submit data to the Google Sheet.
    */
   async handleSubmit() {
+    alert('line 56, jobform.js')
     const formJson = this.utils.formToObj(this.form);
 
     const saveButtonId = '#saveData';
@@ -85,6 +86,17 @@ export default class JobForm {
         this.utils.appendMessage('#result', 'Error submitting data');
         saveButton.textContent = 'Save Data';
       });
+      alert('line 89, jobform.js')
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "dismissJob"}, function(response) {
+            if (response && response.result === "success") {
+                console.log("Dismiss job action was successful.");
+            } else {
+                console.log("Dismiss job action failed or the content script is not active.");
+            }
+        });
+    });
+
   }
 
   /**
